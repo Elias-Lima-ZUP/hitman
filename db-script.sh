2574f5770058b93a62c064a5a97d39a7546e211b
Elias-Lima-ZUP <elias.lima@zup.com.br>
2023-04-27 14:41:54 -0300

    add volume tag to docker command

--- db-script.sh
+++ db-script.sh
@@ -1,2 +1,5 @@
+# Comando para iniciar mysql db
 docker run -e MYSQL_USER=hitman -e MYSQL_PASSWORD=hitman -e MYSQL_DATABASE=hitman-db -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 mysql
 
+# Comando para iniciar mysql db com volume
+docker run -p 3306:3306 -e MYSQL_USER=hitman -e MYSQL_PASSWORD=hitman -e MYSQL_DATABASE=hitman-db -e MYSQL_ROOT_PASSWORD=password --volume db-data-volume:/app mysql
