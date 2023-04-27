434a3c30a54c4c804f241b79527dbdffbc1cf310
Elias-Lima-ZUP <elias.lima@zup.com.br>
2023-04-27 15:03:33 -0300

    add commands to inspect container

--- db-script.sh
+++ db-script.sh
@@ -1,5 +1,12 @@
 # Comando para iniciar mysql db
-docker run -e MYSQL_USER=hitman -e MYSQL_PASSWORD=hitman -e MYSQL_DATABASE=hitman-db -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 mysql
+docker run -p 3306:3306 --name mysql -e MYSQL_USER=hitman -e MYSQL_PASSWORD=hitman -e MYSQL_DATABASE=hitman-db -e MYSQL_ROOT_PASSWORD=password mysql
 
 # Comando para iniciar mysql db com volume
-docker run -p 3306:3306 -e MYSQL_USER=hitman -e MYSQL_PASSWORD=hitman -e MYSQL_DATABASE=hitman-db -e MYSQL_ROOT_PASSWORD=password --volume db-data-volume:/var/lib/mysql mysql
+docker run -p 3306:3306 --name mysql -e MYSQL_USER=hitman -e MYSQL_PASSWORD=hitman -e MYSQL_DATABASE=hitman-db -e MYSQL_ROOT_PASSWORD=password --volume db-data-volume:/var/lib/mysql mysql
+
+# Comandos para inspecionar um container
+docker inspect mysql
+
+docker stats mysql
+
+docker top mysql
